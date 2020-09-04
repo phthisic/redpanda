3059b3e12331359f7d961faa597150eee111174c
Rio412964527 <70362733+Rio412964527@users.noreply.github.com>
2020-09-04 16:27:55 +1000

    Add files via upload

---
+++ 7381 timetable/icons/Myscript.js
@@ -0,0 +1,14 @@
+setInterval("show_time0.innerHTML=new Date().toLocaleString()+' DayofWeek'+'7123456'.charAt(new Date().getDay());",1000); 
+
+$(".click").click(function(){
+        $(".leftmenu").show();
+    });
+$(".leftmenuimg").click(function(){
+        $(".leftmenu").hide();
+    });
+
+$(".img1").click(function(){
+        $(".leftmenu").show();
+    });
+
+
